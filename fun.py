c71d7acd1e30e3bca5e2ef766f8c57c5cbaecadc
Om Doshi <omdoshi108@gmail.com>
2024-12-25 09:52:58 +0530

    Funtion commit

---
+++ fun.py
@@ -0,0 +1,16 @@
+# def welcome(msg):
+#     #to welcome in jungle
+#    # for i in range(n):
+#        # print("Welcome to the jungle")
+#     return msg
+
+
+# msg=welcome('we are in a jungle')
+# print(msg+"Hahaha")
+
+
+def hello(name, age=50):
+    print("Hello "+name+"age is"+str(age))
+
+
+hello("Om", 20)
